5b72e1af18ef4ca65c5eb4e328b765defabd86c3
AapoLempio <aapo.lempio@hotmail.fi>
2020-12-02 18:50:58 +0200

    test

--- test/add.test.js
+++ test/add.test.js
@@ -1,5 +1,17 @@
-describe('Placeholder', () =>{
-    test('placeholder', () =>{
-        
-    });
+import add from '../src/add'
+
+describe('add', () =>{
+  test('addition correct output', () =>{
+    expect(add(1, 1)).toBe(2);
+    expect(add(1, -1)).toBe(0);
+    expect(add(-1, 1)).toBe(0);
+    expect(add(-1, -1)).toBe(-2);
+    expect(add(1234243423,1234243432)).toBe(2468486855);
+  });
+
+  test('missing params result in default value', () =>{
+    expect(add()).toBe(10);
+    expect(add(1, null)).toBe(1);
+    expect(add(null, 1)).toBe(1);
+  });
 });
