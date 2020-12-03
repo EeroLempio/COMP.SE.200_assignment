db2ce3ee37b1e4abda74cd4fc427d064ec792f33
Aapo Lempiö <aapo.lempio@hotmail.fi>
2020-12-03 17:57:32 +0200

    02 first tests (#3)
    * 02: Remove environment variables from test command
    
    * test
    
    * created tests for some library functions
    
    * removed commented code
    
    * removed commented code 2.0

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,5 +1,8 @@
-describe('Placeholder', () =>{
-  test('placeholder', () =>{
-      
+import capitalize from "../src/capitalize"
+
+describe('capitalize', () =>{
+  test('First to capital rest to lower', () =>{
+      expect(capitalize("fOObAR")).toBe("Foobar");
+      expect(capitalize("-fOObAR")).toBe("-foobar");
   });
 });
