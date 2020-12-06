5642ebee41bfa933673893d18b24147716786215
Eero Lempiö <eero.lempio@tuni.fi>
2020-12-06 18:29:35 +0200

    10: Comment the terraform file

--- coverage_s3.tf
+++ coverage_s3.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+    access_key = "YOUR ACCESS KEY HERE"
+    secret_key = "YOUR SECRET KEY HERE"
+    region = "eu-west-1"
+}
+
+/*
+Resource for the actual bucket to be created.
+Public read permissions and static website hosting configured.
+*/
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "comp.se.200-coverage-reports"
   acl    = "public-read"
@@ -7,6 +17,10 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
+/*
+Policy for the bucket, all user groups under the account
+have the rights to the listed actions for the bucket.
+*/
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.s3_bucket.id
 
@@ -35,22 +49,39 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
 POLICY
 }
 
+/*
+IAM user for travis
+*/
 resource "aws_iam_user" "iam_user" {
   name = "travis"
 }
 
+/*
+Access key for the created IAM user
+*/
 resource "aws_iam_access_key" "iam_access_key" {
   user = aws_iam_user.iam_user.name
 }
 
+/*
+Output the access key id
+*/
 output "aws_iam_access_key_id" {
   value = aws_iam_access_key.iam_access_key.id
 }
 
+/*
+Output the access key secret
+*/
 output "aws_iam_access_key_secret" {
   value = aws_iam_access_key.iam_access_key.secret
 }
 
+/*
+Attach policy to the created user with listed rights to the
+created bucket, listing of these is optional as the user
+already has these rights through the bucket policy
+*/
 resource "aws_iam_user_policy" "iam_user_policy" {
   name = "upload_coverage"
   user = aws_iam_user.iam_user.name
